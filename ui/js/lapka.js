4405b9c4bc3f0704fbe5f49e07216423ff48496b
Grzegorz Janik <glujan@users.noreply.github.com>
2017-03-13 17:38:28 +0100

    UI: animal profile stub

--- ui/js/lapka.js
+++ ui/js/lapka.js
@@ -2,12 +2,15 @@ window.onload = function () {
 
     var app,
         animalData = {
+            id: '',
             name: '',
             age: 0,
             place:'' ,
-            description: ''
+            description: '',
+            photos: []
         },
-        baseUrl = window.location.protocol + "//" + window.location.host + "/api/";
+        baseUrl = "/",
+        userId = 'user-id';  // TODO Set to real id
 
     Vue.component('custom-header', {
         template: "<header>I'm a header</header>"
@@ -17,11 +20,84 @@ window.onload = function () {
         template: "<footer>I'm a footer</footer>"
     });
 
+    function httpError(url) {
+        return function (error) {
+            console.warn("Could not fetch data from", url, ":", error);
+        };
+    }
+
     Vue.component('animal-profile', {
         // XXX Other ways: http://stackoverflow.com/q/31633573
         template: '#animal-profile-tmpl',
         data: function () {
-            return animalData;
+            return {
+                animal: animalData,
+                next: null,
+                matching: [],  // list of animals ids
+                // lru: {}
+            };
+        },
+        methods: {
+            _get: function (url, success) {
+                var url = baseUrl + url + '/';
+                axios.get(url)
+                  .then(success)
+                  .catch(httpError(url));
+            },
+            _fetchMatching: function (success) {
+                var $this = this;
+                this._get('animal/matching/' + userId, function (resp) {
+                    $this.matching = resp.data;
+                    if (typeof success === 'function') {
+                        success(resp)
+                    }
+                });
+
+            },
+            like: function (uid, e) {  // TODO Implement
+                window.alert("Liked " + uid);
+            },
+            seeNext: function (curr_uid) {
+                var $this = this;
+                function switchAnimals() {
+                    $this.animal = $this.next;
+                    $this.next = null;
+                }
+
+                if (curr_uid) {
+                    axios.post(baseUrl + 'animal/skip/' + userId + '/');
+                }
+
+                if (this.next === null) {
+                    this.fetchNext(switchAnimals);
+                } else {
+                    switchAnimals();
+                }
+            },
+            fetchNext: function (success) {
+                if (this.next !== null) {
+                    return;
+                }
+
+                var $this = this,
+                    uid = this.matching.shift();
+
+                if (uid === undefined) {
+                    return;
+                }
+                this._get('animal/' + uid, function(resp) {
+                    $this.next = resp.data;
+                    if (typeof success === 'function') {
+                        success(resp)
+                    }
+                });
+            }
+        },
+        mounted: function () {
+            var $this = this;
+            this._fetchMatching(function () {
+                $this.seeNext();
+            });
         }
     });
 
@@ -89,7 +165,7 @@ window.onload = function () {
     animalData.description = 'Naughty dog!';
 
     app = new Vue({
-        el: '#app',
+        el: '#animal-app',
         data: {
             message: 'Your profile',
         }
